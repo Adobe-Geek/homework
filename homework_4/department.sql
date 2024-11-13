00270c54d6b944fe62120349e3ea457f01f325bf
Ania Komarova <anita.komarova@gmail.com>
2024-11-13 17:56:12 +0100

    Fix manager_id and position_id fields

--- homework_4/department.sql
+++ homework_4/department.sql
@@ -2,5 +2,5 @@ create table departments(
 	id SERIAL primary key,
 	name VARCHAR(50) not null,
 	abbreviation VARCHAR(10),
-	manager VARCHAR(50)
+	manager_id INT references employees(id)
 );
--- homework_4/positions_history.sql
+++ homework_4/positions_history.sql
@@ -1,7 +1,6 @@
 create table positions_history(
 	id SERIAL primary key,
-	employee_id INT references employees(id),
-	position INT references positions(id),
+	position_id INT references positions(id),
 	start_date DATE,
 	end_date DATE
 );
